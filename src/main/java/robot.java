daa28450b999530f1291431fab2e52e3435319d9
Chuck Cleason <spacebutterflytytoalba@gmail.com>
2017-03-04 13:55:51 -0500

    Trying sample robot tank drive

--- src/main/java/robot.java
+++ src/main/java/robot.java
@@ -1,5 +1,30 @@
-/**
+/*
  * Created by 3322 - Programming 3 on 3/2/2017.
  */
-public class robot {
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class robot extends SampleRobot {
+
+    RobotDrive myDrive;
+    Joystick left, right;
+
+    public void robotInit() {
+        myDrive = new RobotDrive(2, 3, 1, 0);
+        left = new Joystick(1);
+        right = new Joystick(2);
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+
+    public void operatorControl() {
+        while (isOperatorControl() && isEnabled()) {
+            myDrive.tankDrive(left, right);
+            Timer.delay(0.01);
+        }
+    }
+
 }
